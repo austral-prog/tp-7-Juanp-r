5675337bb2026ef72cb94e7192a3c09607a4a8df
Juan Prieto <162722098+Juanp-r@users.noreply.github.com>
2024-05-23 22:35:44 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,4 +1,8 @@
 def index_of_by_index(word, list, index):
+    for forindex, search_word in enumerate(list):
+        if in
+                word in search_word):
+            return index
     return -1
 
 
@@ -7,7 +11,11 @@ def index_of_empty(list):
 
 
 def index_of(word, list):
+    for index, search_word in enumerate(list):
+        if word in search_word:
+            return index
     return -1
+
 
 
 def put(word, list):
