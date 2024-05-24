460cf7f0e1067c8684fb0c17de54ba3b4f31d018
Juan Prieto <162722098+Juanp-r@users.noreply.github.com>
2024-05-23 22:38:20 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,16 @@
 def enumerate_list(list):
-    return list
+    temp_list = []
+    for elemento in list:
+        if elemento != "":
+            temp_list.append(f'{len(temp_list)}. {elemento}')
+    return temp_list
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(other_list):
+    temp_list = []
+    for index, color in enumerate(other_list):
+        if other_list[index] != "":
+            temp_list.append(f'{len(temp_list)}. {color[::-1]}')
+    return temp_list
+print(enumerate_list(list_v1))
+print(enumerate_backwards(list_v2))
